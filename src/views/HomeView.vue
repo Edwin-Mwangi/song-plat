e1ebe8e493cd993cdcc48b37f05db3f49009928c
Edwin Mwangi <edwinmwangi138@gmail.com>
2022-06-22 21:02:46 +0300

    gettin realtime playlist challenge

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="home">
     <p>Homepage</p>
+    <div v-if="error" class="error">Could not fetch data {{ error }}</div>
+    <div v-if="documents">
+      <div v-for="doc in documents" :key="doc.id">{{doc.title}}</div>
+    </div>
   </div>
 </template>
 
 <script>
+import getCollection from '../composables/getCollection'
 
 export default {
   name: 'HomeView',
+  setup() {
+    const { documents, error } = getCollection('playlists')
+
+  return { documents, error }
+  }
 }
 </script>
